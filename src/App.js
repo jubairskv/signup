325c0c622ad59e0144b79120a5bffed511837553
jubairskv <jubairskv@gmail.com>
2024-08-20 22:13:39 +0530

    Signup page using localstorage and create browser router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-
-import './App.css';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./App.css";
+import SignIn from "./Components/SignIn";
+import Signup from "./Components/Signup";
+import Body from "./Components/Body";
+import ForgotPassword from "./Components/ForgotPassword";
 
 function App() {
+  const appRouter = createBrowserRouter([
+    {
+      path: "/",
+      element: <SignIn />,
+    },
+    {
+      path: "/signup",
+      element: <Signup />,
+    },
+    {
+      path: "/home",
+      element: <Body />,
+    },
+    {
+      path: "forgotpassword",
+      element: <ForgotPassword />,
+    },
+  ]);
   return (
     <div className="App">
-      hi
+      <RouterProvider router={appRouter} />
     </div>
   );
 }
