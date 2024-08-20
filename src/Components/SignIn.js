b25fc48df55dbc84d0129222e2c18a776dbdc048
jubairskv <jubairskv@gmail.com>
2024-08-20 22:43:49 +0530

    Signup page using localstorage and create browser router

--- src/Components/SignIn.js
+++ src/Components/SignIn.js
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const SignIn = () => {
-  return (
-    <div>SignIn</div>
-  )
-}
+  const [input, setInput] = useState({
+    email: "",
+    password: "",
+  });
 
-export default SignIn
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Email</label>
+          <input
+            type="text"
+            value={input}
+            placeholder="Enter the email"
+            onChange={(e) =>
+              setInput({ ...input, [e.target.email]: e.target.value })
+            }
+          />
+        </div>
+        <div>
+          <label>password</label>
+          <input
+            type="text"
+            value={input}
+            placeholder="Enter the password"
+            onChange={(e) =>
+              setInput({ ...input, [e.target.password]: e.target.value })
+            }
+          />
+        </div>
+        <div>
+          <button>Login</button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default SignIn;
