ac27f342d172880af40987cda7c3a7074d1ea4ba
jubairskv <jubairskv@gmail.com>
2024-08-20 23:51:44 +0530

    Signup page using localstorage and create browser router

--- src/Components/ForgotPassword.js
+++ src/Components/ForgotPassword.js
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const ForgotPassword = () => {
-  return (
-    <div>ForgotPassword</div>
-  )
-}
+  const [email, setEmail] = useState("");
+  const [newPassword, setNewPassword] = useState("");
 
-export default ForgotPassword
+  const handleResetPassword = () => {
+    const storedUser = JSON.parse(localStorage.getItem("user"));
+    console.log(storedUser);
+    if (storedUser && email === storedUser.email) {
+      const updatedUser = { ...storedUser, password: newPassword };
+      localStorage.setItem("user", JSON.stringify(updatedUser));
+      alert("Password reset successfully!");
+    } else {
+      alert("Email not found!");
+    }
+  };
+
+  return (
+    <div>
+      <div>
+        <h2>Forgot Password</h2>
+        <input
+          type="email"
+          placeholder="Enter your email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <div>
+        <input
+          type="password"
+          placeholder="Enter new password"
+          value={newPassword}
+          onChange={(e) => setNewPassword(e.target.value)}
+        />
+      </div>
+
+      <button onClick={handleResetPassword}>Reset Password</button>
+    </div>
+  );
+};
+
+export default ForgotPassword;
