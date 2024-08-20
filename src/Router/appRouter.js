544e941d9203a26d908271d478fc2fd93de58c3f
jubairskv <jubairskv@gmail.com>
2024-08-20 22:14:14 +0530

    Signup page using localstorage and create browser router

--- src/Router/appRouter.js
+++ src/Router/appRouter.js
@@ -1,28 +1,27 @@
-import React from "react";
-import { createBrowserRouter } from "react-router-dom";
-import SignIn from "../Components/SignIn";
-import Signup from "../Components/Signup";
-import Body from "../Components/Body";
-import ForgotPassword from "../Components/ForgotPassword";
+// import React from "react";
+// import { createBrowserRouter } from "react-router-dom";
+// import SignIn from "../Components/SignIn";
+// import Signup from "../Components/Signup";
+// import Body from "../Components/Body";
+// import ForgotPassword from "../Components/ForgotPassword";
 
-  const appRouter = createBrowserRouter([
-    {
-      path: "/",
-      element: <SignIn />,
-    },
-    {
-      path: "/signup",
-      element: <Signup />,
-    },
-    {
-      path: "/home",
-      element: <Body />,
-    },
-    {
-      path: "forgotpassword",
-      element: <ForgotPassword />,
-    },
-  ]);
- 
+// const appRouter = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <SignIn />,
+//   },
+//   {
+//     path: "/signup",
+//     element: <Signup />,
+//   },
+//   {
+//     path: "/home",
+//     element: <Body />,
+//   },
+//   {
+//     path: "forgotpassword",
+//     element: <ForgotPassword />,
+//   },
+// ]);
 
-export default appRouter;
+// export default appRouter;
